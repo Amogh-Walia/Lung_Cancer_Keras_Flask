d05da1a2a85ba77bc7e2482e5cf2e40158febfb4
Amogh-Walia <72308844+Amogh-Walia@users.noreply.github.com>
2022-01-02 20:17:49 +0530

    Add files via upload

--- Keras.py
+++ Keras.py
@@ -6,11 +6,21 @@ import numpy as np
 
 
 model = load_model('cancerCNN.h5')
-print(model.summary())
-img = image.load_img('./upload\person1946_bacteria_4875.jpeg', target_size=(224, 224))
+config = model.get_config() # Returns pretty much every information about your model
+print(config["layers"][0]["config"]["batch_input_shape"]) # returns a tuple of width, height and channels
+img = image.load_img('./upload\person1946_bacteria_4875.jpeg', target_size=(40, 40),color_mode="grayscale")
 img_array = image.img_to_array(img)
 img_batch = np.expand_dims(img_array, axis=0)
-img_preprocessed = preprocess_input(img_batch)
-prediction = model.predict(img_preprocessed)
+#img_preprocessed = preprocess_input(img_batch)
+
+
+for i in range(0,len(img_batch[0][0])):
+    img_batch[0][0][i][0] = img_batch[0][0][i][0]*(1/255)
+
+
+
+
+      
+prediction = model.predict(img_batch)
 
 print(prediction)
